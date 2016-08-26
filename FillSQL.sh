fff78e9d44c4d425ef8a4ff96d93278be144eca8
Noah Nelson <nelson.noah96@gmail.com>
2016-08-25 19:18:29 -0700

    started a new script to fill a sqlite database with fingerprints

---
+++ FillSQL.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+echo "Printing all FULL wav files to sqlite file $1"
+
+SONGID=1
+for f in TestSet/*FULL.wav
+do
+    echo "Fingerprinting $f with songId $SONGID"
+    ./Fingerprinter $f $SONGID > "${f%.wav}ID.csv"
+    ((SONGID++))
+done
+
+for f in TestSet/*ID.csv
+do
+    echo "Inserting $f into sqlite database at $1"
+    echo ".mode csv;.import $f fingerprints;" | sqlite3 $1
+done
